c18521133814a4d0ab8c204034e23847c274c375
cutm <debendra.maharana@cutm.ac.in>
2024-09-16 22:48:35 +0530

    this code is commit by me

---
+++ RashuGitProject/src/com/rashu/rashuGithub.java
@@ -0,0 +1,12 @@
+package com.rashu;
+
+public class rashuGithub {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	System.out.println("Wel come to the GitHub");
+	System.out.println("Wel come to the GitHub");
+
+		}
+
+	}
---
+++ RashuGitProject/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module RashuGitProject {
+}
